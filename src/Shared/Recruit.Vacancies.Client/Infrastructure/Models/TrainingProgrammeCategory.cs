ba52e40b18137688da8f0e7a42986fe8f4f97675
sachin.patel <sachin.patel@valtech.co.uk>
2018-04-25 10:22:18 +0100

    ER-377 :sparkle: - Adding class for TrainingProgrammeCategory which will be used for lookup by V2 vacancy ETL process.

---
+++ src/Shared/Recruit.Vacancies.Client/Infrastructure/Models/TrainingProgrammeCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace Esfa.Recruit.Vacancies.Client.Infrastructure.Models
+{
+    public class TrainingProgrammeCategory
+    {
+        public string Id { get; set; }
+        public string Category { get; set; }
+        public string CategoryCode { get; set; }
+        public string SubCategory { get; set; }
+        public string SubCategoryCode { get; set; }
+    }
+}
