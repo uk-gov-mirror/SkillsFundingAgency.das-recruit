9b41658a2e35f4ddedfda6ececdf68aae163d7f7
sachin.patel <sachin.patel@valtech.co.uk>
2019-01-10 11:59:40 +0000

    :hammer: - Using App Insights JS partial and NWebSec the same way on QA as Employer and Provider Recruit +semver: skip

--- src/QA/QA.Web/Startup.ConfigureServices.cs
+++ src/QA/QA.Web/Startup.ConfigureServices.cs
@@ -2,6 +2,7 @@
 using Esfa.Recruit.Qa.Web.Mappings;
 using Esfa.Recruit.Qa.Web.Orchestrators;
 using Esfa.Recruit.Qa.Web.Security;
+using Esfa.Recruit.Shared.Web.Configuration;
 using Esfa.Recruit.Shared.Web.RuleTemplates;
 using Esfa.Recruit.Shared.Web.Services;
 using Microsoft.AspNetCore.DataProtection;
@@ -36,6 +37,8 @@ namespace Esfa.Recruit.Qa.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApplicationInsightsConfiguration>(_configuration.GetSection("ApplicationInsights"));
+
             //A service provider for resolving services configured in IoC
             var sp = services.BuildServiceProvider();
 
@@ -60,7 +63,7 @@ namespace Esfa.Recruit.Qa.Web
                 }
             );
 
-            services.AddApplicationInsightsTelemetry(_configuration);
+            services.AddApplicationInsightsTelemetry(_configuration); // may need to remove
             services.AddAuthenticationService(_authenticationConfig);
             services.AddAuthorizationService(_legacyAuthorizationConfig, _authorizationConfig);
 
